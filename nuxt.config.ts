801e11ad574aca6f8f60974db2e0194a58eec0d9
Yanek-K <yanek.dev@gmail.com>
2022-10-11 13:45:12 -0400

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    FIREBASE_API_KEY: "process.env.FIREBASE_API_KEY",
+  },
+  privateRuntimeConfig: {
+    FIREBASE_API_KEY: "process.env.FIREBASE_API_KEY",
+  },
+});
---
+++ plugins/firebaseAuth.client.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+
+  const firebaseConfig = {
+    apiKey: config.FIREBASE_API_KEY,
+  };
+
+  const app = initializeApp(firebaseConfig);
+  console.log(app);
+});
